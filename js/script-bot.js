3dc51e27acaa2f7930ce7ba6d4a351214f3493a7
Ryan-Rrs <ryan.obmep@gmail.com>
2024-08-27 16:21:12 -0300

    Alteração no bot
    Corrigido bug onde era possível clicar várias vezes

--- js/script-bot.js
+++ js/script-bot.js
@@ -2,17 +2,21 @@ const selecBox = document.querySelector(".select-box"),
 selectXBtn = selecBox.querySelector(".option .playerX"),
 selectOBtn = selecBox.querySelector(".option .playerO"),
 playBoard = document.querySelector(".play-board"),
-playMode = document.querySelector(".mode"),
 allBox = document.querySelectorAll("section span"),
 players = document.querySelector(".players"),
 resultBox = document.querySelector(".result-box"),
 wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
+let playerXIcon = "fas fa-times";
+let playerOIcon = "far fa-circle";
+let playerSign = "X";
+let runBot = true;
+let isBotTurn = false; // Variável para controlar o turno do bot
+
 window.onload = ()=> {
     for (let i = 0; i < allBox.length; i++) {
         allBox[i].setAttribute("onclick","clickedBox(this)");
-        
     }
 
     selectXBtn.onclick = ()=>{
@@ -27,58 +31,43 @@ window.onload = ()=> {
     }
 }
 
-let playerXIcon = "fas fa-times";
-let playerOIcon = "far fa-circle";
-let playerSign = "X";
-let runBot = true;
-
 function clickedBox(element){
-    if(players.classList.contains("player")){
-        playerSign = "O";
-        element.innerHTML = `<i class="${playerOIcon}"></i>`;
-        players.classList.remove("active");
+    if (!isBotTurn && element.childElementCount == 0) { // Garantir que o jogador não jogue durante o turno do bot
+        playerSign = players.classList.contains("player") ? "O" : "X";
+        element.innerHTML = `<i class="${playerSign === "X" ? playerXIcon : playerOIcon}"></i>`;
+        players.classList.toggle("active");
         element.setAttribute("id", playerSign);
-    }else{
-        playerSign = "X";
-        element.innerHTML = `<i class="${playerXIcon}"></i>`;
-        players.classList.add("active");
-        element.setAttribute("id", playerSign);
+        element.style.pointerEvents = "none";
+        selectWinner();
+
+        if (runBot) {
+            isBotTurn = true;
+            setTimeout(() => {
+                bot();
+            }, 500); // Reduzi o delay para 500ms para melhor resposta
+        }
     }
-    selectWinner();
-    element.style.pointerEvents = "none";
-    let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
-    
-    setTimeout(()=>{
-        bot(runBot);
-    },randomDelayTime);
 }
 
-function bot(runBot){
-        if(runBot){
-        let array = [];
-        playerSign = "O";
-        for (let i = 0; i < allBox.length; i++) {
-            if(allBox[i].childElementCount == 0){
-                array.push(i);
-            }
-            
+function bot(){
+    let availableBoxes = [];
+    playerSign = "O";
+    for (let i = 0; i < allBox.length; i++) {
+        if (allBox[i].childElementCount == 0) {
+            availableBoxes.push(i);
         }
-        let randomBox = array[Math.floor(Math.random() * array.length)];
-        if(array.length > 0){
-            if(players.classList.contains("player")){
-                playerSign = "X";
-                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-                players.classList.add("active");
-                allBox[randomBox].setAttribute("id", playerSign);
-            
-            }else{
-                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-                players.classList.remove("active");
-                allBox[randomBox].setAttribute("id", playerSign);
-            } 
-            selectWinner(); 
-        }
-        allBox[randomBox].style.pointerEvents = "none";
+    }
+
+    if (availableBoxes.length > 0) {
+        let randomBox = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
+        let selectedBox = allBox[randomBox];
+        playerSign = players.classList.contains("player") ? "X" : "O";
+        selectedBox.innerHTML = `<i class="${playerSign === "X" ? playerXIcon : playerOIcon}"></i>`;
+        players.classList.toggle("active");
+        selectedBox.setAttribute("id", playerSign);
+        selectedBox.style.pointerEvents = "none";
+        selectWinner();
+        isBotTurn = false; // Libera para o jogador jogar novamente
     }
 }
 
@@ -86,34 +75,28 @@ function getClass(idname){
     return document.querySelector(".box"+ idname).id;
 }
 
-function checkClass(val1,val2,val3,sign){
-    if(getClass(val1) == sign && getClass(val2) == sign && getClass(val3) == sign){
-        return true;
-    }
+function checkClass(val1, val2, val3, sign){
+    return getClass(val1) === sign && getClass(val2) === sign && getClass(val3) === sign;
 }
 
 function selectWinner(){
-    if(checkClass(1,2,3,playerSign) || checkClass(4,5,6,playerSign) || checkClass(7,8,9,playerSign) || checkClass(1,4,7,playerSign) || checkClass(2,5,8,playerSign) || checkClass(3,6,9,playerSign) || checkClass(1,5,9,playerSign) || checkClass(3,5,7,playerSign))
-    {
+    if(checkClass(1, 2, 3, playerSign) || checkClass(4, 5, 6, playerSign) || checkClass(7, 8, 9, playerSign) || 
+       checkClass(1, 4, 7, playerSign) || checkClass(2, 5, 8, playerSign) || checkClass(3, 6, 9, playerSign) || 
+       checkClass(1, 5, 9, playerSign) || checkClass(3, 5, 7, playerSign)) {
+
         runBot = false;
-        bot(runBot);
         setTimeout(()=>{
             playBoard.classList.remove("show");
             resultBox.classList.add("show");
-        },700);
+        }, 700);
         wonText.innerHTML = `Jogador <p>${playerSign}</p> Ganhou!`;
-    }else{
-
-        if(getClass(1) != "" && getClass(2) != "" && getClass(3) != "" && getClass(4) != "" && getClass(5) != "" && getClass(6) != "" && getClass(7) != "" && getClass(8) != "" && getClass(9) != ""  ){
-            runBot = false;
-            bot(runBot);
-            setTimeout(()=>{
-                playBoard.classList.remove("show");
-                resultBox.classList.add("show");
-            },700);
-            wonText.textContent = `Jogo Empatou!`;
-        }
-
+    } else if ([...allBox].every(box => box.childElementCount > 0)) {
+        runBot = false;
+        setTimeout(()=>{
+            playBoard.classList.remove("show");
+            resultBox.classList.add("show");
+        }, 700);
+        wonText.textContent = `Jogo Empatou!`;
     }
 }
 
